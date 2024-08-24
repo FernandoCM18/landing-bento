5163262e21e4f1c5b3d716608dbab35e560dd40c
FernandoCM18 <fernando.c.m@live.com>
2024-08-24 16:01:02 -0700

    chore: Update tailwind.config.ts to include sections directory for theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,19 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'landing': '#202020',
-      'text-primary': 'rgba(248, 248, 248, 0.95)',
-      'text-secondary': 'rgba(248, 248, 248, 0.70)',
-      'text-tertiary': 'rgba(248, 248, 248, 0.50)',
-      'stroke-25-button': 'rgba(255, 255, 255, 0.40)',
-      'surface-background': 'rgba(40, 40, 40, 0.70)',
+    extend: {
+      colors: {
+        'landing': '#202020',
+        'text-primary': 'rgba(248, 248, 248, 0.95)',
+        'text-secondary': 'rgba(248, 248, 248, 0.70)',
+        'text-tertiary': 'rgba(248, 248, 248, 0.50)',
+        'stroke-25-button': 'rgba(255, 255, 255, 0.40)',
+        'surface-background': 'rgba(40, 40, 40, 0.70)',
+      },
     }
   },
   plugins: [],
