16012475e06be48851de75fed79ac69889a40dd2
FernandoCM18 <fernando.c.m@live.com>
2024-08-24 16:04:42 -0700

    feat: Add tasks, users, groups, pricing, and faq data files

---
+++ src/data/users.ts
@@ -0,0 +1,47 @@
+export const users = [
+  {
+    id: "0",
+    name: "Emma L.",
+    avatar: "/assets/images/avatar-1.png",
+  },
+  {
+    id: "1",
+    name: "Samantha K.",
+    avatar: "/assets/images/avatar-2.png",
+  },
+  {
+    id: "2",
+    name: "Ryan M.",
+    avatar: "/assets/images/avatar-3.png",
+  },
+  {
+    id: "3",
+    name: "David T.",
+    avatar: "/assets/images/avatar-5.png",
+  },
+  {
+    id: "4",
+    name: "Lila K.",
+    avatar: "/assets/images/avatar-8.png",
+  },
+  {
+    id: "5",
+    name: "Carlos G.",
+    avatar: "/assets/images/avatar-9.png",
+  },
+  {
+    id: "6",
+    name: "Margaret W.",
+    avatar: "/assets/images/avatar-1.png",
+  },
+  {
+    id: "7",
+    name: "Lauren T.",
+    avatar: "/assets/images/avatar-2.png",
+  },
+  {
+    id: "8",
+    name: "Jake L.",
+    avatar: "/assets/images/avatar-3.png",
+  },
+];
