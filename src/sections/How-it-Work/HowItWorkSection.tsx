0676cd514b1c6400b2d85484e5680b9522717894
FernandoCM18 <fernando.c.m@live.com>
2024-08-25 11:39:17 -0700

    feat: Add Card component and update HowItWorkSection to display cards for each step

--- src/sections/How-it-Work/HowItWorkSection.tsx
+++ src/sections/How-it-Work/HowItWorkSection.tsx
@@ -1,4 +1,5 @@
-import { TagLine, TaskGroup, Typography } from "@/components"
+import { Card, TagLine, TaskGroup, Typography } from "@/components"
+import { steps } from "@/data/steps";
 
 export const HowItWorkSection = () => {
   return (
@@ -15,7 +16,23 @@ export const HowItWorkSection = () => {
         `}
         />
       </div>
-      <TaskGroup />
+      <div className="flex flex-col justify-center items-center gap-5">
+        <TaskGroup />
+        <div className="grid grid-cols-3 gap-5 justify-center items-center">
+          {
+            steps.map((step) => (
+              <Card
+                key={step.id}
+                image={step.image!}
+                title={step.title}
+                description={step.description}
+                number={step.id}
+              />
+            ))
+          }
+        </div>
+
+      </div>
 
     </section>
   )
