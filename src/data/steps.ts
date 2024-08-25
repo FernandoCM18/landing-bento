a0b9dac41e96c98ee3c316fc1ec06889a362dc26
FernandoCM18 <fernando.c.m@live.com>
2024-08-25 11:39:10 -0700

    feat: Add steps data file

---
+++ src/data/steps.ts
@@ -0,0 +1,20 @@
+export const steps = [
+  {
+    id: 1,
+    title: 'Install SimpleList',
+    description: 'Create, manage, and conquer your to-do lists with ease',
+    image: '/assets/images/install-illustration.png',
+  },
+  {
+    id: 2,
+    title: 'Create your first list',
+    description: "Tap the '+' button and start adding items to your list",
+    image: '/assets/images/app-illustration.png',
+  },
+  {
+    id: 3,
+    title: 'Stay organized effortlessly',
+    description: 'Manage, update, and complete tasks with ease',
+    image: '/assets/images/financial-planning-illustration.png',
+  }
+];
