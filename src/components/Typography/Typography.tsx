3d3ee30609f2be140f67210804da6c0ca606156a
FernandoCM18 <fernando.c.m@live.com>
2024-08-29 14:49:58 -0700

    refactor: Update SVG stroke attributes in TaskGroup component

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -31,17 +31,19 @@ interface Props {
 export const Typography = ({ text, variant, className }: Props) => {
   switch (variant) {
     case 'H1':
-      return <h1 className={`${grotesk.className} font-bold text-[64px] -tracking-[0.25px]`}>{text}</h1>;
+      return <h1 className={`${grotesk.className} font-bold text-[64px] -tracking-[0.25px] ${className}`}>{text}</h1>;
     case 'H2':
       return <h2 className={`${grotesk.className} font-bold text-5xl leading-[56px] -tracking-[-0.96px] ${className}`}>{text}</h2>;
+    case 'H3':
+      return <h3 className={`${grotesk.className} font-bold text-[40px] leading-[48px] -tracking-[0.8px] ${className}`}>{text}</h3>
     case 'Subtitle':
-      return <p className={`${rubik.className} text-[20px] leading-8 font-light text-text-secondary`}>{text}</p>
+      return <p className={`${rubik.className} text-[20px] leading-8 font-light ${className}`}>{text}</p>
     case 'Base2(SM)':
-      return <span className={`${rubik.className} text-sm font-semibold z-10`}>{text}</span>
+      return <span className={`${rubik.className} text-sm font-semibold z-10 ${className}`}>{text}</span>
     case 'Small':
-      return <p className={`${rubik.className} text-[10px] leading-3 text-text-tertiary`}>{text}</p>
+      return <p className={`${rubik.className} text-[10px] leading-3 ${className}`}>{text}</p>
     case 'Body':
-      return <p className={`${inter.className} leading-6 text-text-tertiary`}>{text}</p>
+      return <p className={`${inter.className} leading-6 ${className}`}>{text}</p>
     case 'Harline':
       return <p className={`${rubik.className} font-semibold text-xs leading-5 tracking-[0.6px] ${className}`}>{text}</p>
     case 'Title':
