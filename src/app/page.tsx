ee40b096a365b47dbd5ef4b2f8541012ee0312e7
FernandoCM18 <fernando.c.m@live.com>
2024-08-29 14:49:21 -0700

    feat: Add KeyFeaturesSection to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { HeroHeaderSection, HowItWorkSection } from "@/sections";
+import { HeroHeaderSection, HowItWorkSection, KeyFeaturesSection } from "@/sections";
 
 export default function Home() {
   return (
     <main className="bg-landing px-40 text-center space-y-40">
       <HeroHeaderSection />
       <HowItWorkSection />
+      <KeyFeaturesSection />
     </main>
   );
 }
