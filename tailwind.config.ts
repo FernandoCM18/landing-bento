85ef93e43da3be31a6a23f747adebc583b8a150f
FernandoCM18 <fernando.c.m@live.com>
2024-08-29 14:48:09 -0700

    refactor: Update Card module CSS and Card component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,14 +18,10 @@ const config: Config = {
         'surface-background': 'rgba(40, 40, 40, 0.70)',
         'neutral-neutral-22': 'rgba(248, 248, 248, 0.02))',
         'stroke-15-card': 'rgba(255, 255, 255, 0.40))',
-
-
-
       },
       boxShadow: {
         'card-dark': '2px 4px 16px 0px rgba(248, 248, 248, 0.06), 0px 24px 24px -16px rgba(5, 5, 5, 0.09), 0px 6px 13px 0px rgba(5, 5, 5, 0.10), 0px 6px 4px -4px rgba(5, 5, 5, 0.10), 0px 5px 1.5px -4px rgba(5, 5, 5, 0.25)',
-
-      }
+      },
     }
   },
   plugins: [],
