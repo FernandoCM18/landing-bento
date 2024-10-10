6b4f51b459f9ec427f2168c1a74f4f455bb1c26a
FernandoCM18 <fernando.c.m@live.com>
2024-10-10 13:54:22 -0700

    refactor: use Icon component

--- src/components/Cicular-icon/CircularIcon.tsx
+++ src/components/Cicular-icon/CircularIcon.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import React from "react"
+import Icon, { IconName } from "../Icon/Icon";
 
 interface Props {
-  icon: string;
+  icon: IconName;
 };
 
 export const CircularIcon = ({ icon }: Props) => {
   return (
     <div className="bg-neutral-neutral-25 p-[10px] rounded-full">
-      <Image src={`assets/icons/${icon}`} alt="icon" width={24} height={24} />
+      <Icon name={icon} />
     </div>
   )
 }
