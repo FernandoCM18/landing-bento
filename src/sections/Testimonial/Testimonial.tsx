081b48a81cdd4828a0d13706888be538c012f8bb
FernandoCM18 <fernando.c.m@live.com>
2024-10-13 22:55:01 -0700

    Refactor Testimonial module styles and add chevron icon

--- src/sections/Testimonial/Testimonial.tsx
+++ src/sections/Testimonial/Testimonial.tsx
@@ -1,17 +1,16 @@
 'use client'
-import Image from "next/image"
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-import "swiper/css/pagination";
-
-import { Navigation, Thumbs, Pagination } from "swiper/modules";
-import { Avatar, Starts, TagLine, Typography } from "@/components"
-import { testimonials } from "@/data/testimonials"
-import style from './Testimonial.module.css'
 import { useState } from "react";
-import Icon from "@/components/Icon/Icon";
+import Image from "next/image";
+import { Avatar, Starts, TagLine, Typography, Icon } from "@/components";
+import { testimonials } from "@/data/testimonials";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import Style from "./Testimonial.module.css";
+import { Autoplay, Navigation, Pagination, Thumbs } from "swiper/modules";
+import { cn } from "@/utils/cn";
 
 export const Testimonial = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
@@ -34,34 +33,40 @@ export const Testimonial = () => {
         <Starts className='absolute h-full' />
         <div className="relative bg-[#181818]/20 w-[872px] h-[316px] rounded-[48px] mx-auto overflow-hidden flex items-center justify-center">
           <Image src="/assets/images/user-list-frame.svg" alt="user-list-frame" layout="fill" objectFit="cover" />
-          <div className={`${style.BackgroundTestimonial} bg-surface-background`}>
+          <div className={`${Style.BackgroundTestimonial} bg-surface-background`}>
             <Swiper
-              // initialSlide={2}
-              className="swiper-container-author"
+              className="swiper-author"
+              spaceBetween={12}
+              slidesPerView={1}
+              loop={true}
+              speed={600}
+              centeredSlides={true}
+              modules={[Navigation, Thumbs, Pagination]}
+              thumbs={{ swiper: thumbsSwiper }}
               navigation={{
                 prevEl: ".swiper-arrow-prev",
                 nextEl: ".swiper-arrow-next",
               }}
               pagination={{
                 el: ".swiper-pagination-testimonials",
-                // clickable: true,
+                clickable: true,
               }}
-              loop={true}
-              speed={600}
-              slidesPerView={1}
-              spaceBetween={12}
-              centeredSlides={true}
-              thumbs={{ swiper: thumbsSwiper }}
-              modules={[Navigation, Thumbs, Pagination]}
               breakpoints={{
                 768: {
                   slidesPerView: 3,
                 },
               }}
+            // autoplay={{
+            //   delay: 2500,
+            //   disableOnInteraction: false,
+            // }}
             >
               {testimonials.map((testimonial) => (
                 <SwiperSlide
-                  className={`${style.BorderTestimonial} bg-neutral-neutral-22 rounded-[20px] p-3 mt-3`}
+                  className={cn(
+                    Style.BorderTestimonial,
+                    Style.AuthorTestimonial,
+                    "bg-neutral-neutral-22 rounded-[20px] p-3 mt-3")}
                   key={testimonial.id}
                 >
                   <div className="flex gap-4">
@@ -76,36 +81,39 @@ export const Testimonial = () => {
                 </SwiperSlide>
               ))}
             </Swiper>
-            <div className="mt-16">
-              <Swiper
-                spaceBetween={10}
-                speed={500}
-                onSwiper={setThumbsSwiper}
-                modules={[Navigation, Thumbs, Pagination]}
-                loop={true}
-              >
-                {testimonials.map((testimonial) => (
-                  <SwiperSlide
-                    key={testimonial.id}
-                  >
-                    <Typography variant="Title" text={testimonial.title} className="text-text-primary" />
-                    {testimonial.content && (
-                      <Typography variant="Title" text={testimonial.content} className="text-text-tertiary" />
-                    )}
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-
-            </div>
+            <Swiper
+              className="mt-16 select-none"
+              spaceBetween={10}
+              speed={500}
+              onSwiper={setThumbsSwiper}
+              modules={[Navigation, Thumbs, Pagination]}
+              loop={true}
+              allowTouchMove={false}
+            // autoplay={{
+            //   delay: 2500,
+            //   disableOnInteraction: false,
+            // }}
+            >
+              {testimonials.map((testimonial) => (
+                <SwiperSlide
+                  key={testimonial.id}
+                >
+                  <Typography variant="Title" text={testimonial.title} className="text-text-primary" />
+                  {testimonial.content && (
+                    <Typography variant="Title" text={testimonial.content} className="text-text-tertiary" />
+                  )}
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
-      <div>
+      <div className="flex gap-4 items-center justify-center w-[200px] h-14 mx-auto p-2">
         <button className="w-10 h-10 swiper-arrow-prev">
           <Icon name="chevronLeft" />
         </button>
 
-        <div className="swiper-pagination-testimonials" />
+        <div className={cn("swiper-pagination-testimonials")} />
 
         <button className="w-10 h-10 swiper-arrow-next">
           <Icon name="chevronRight" />
